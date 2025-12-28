2df752d21582c230cdfc820bcff7315da5c073da
Prashant-penteai <prashant@penteai.com>
2025-12-28 18:36:34 +0530

    added frontend env

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,7 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
-// middlewares
+// middlewares initialization
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
